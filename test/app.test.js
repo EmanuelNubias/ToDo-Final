b8b79cffc27e658adbf2b059fde317d1469aa7e4
EmanuelNubias <emanuel.singer00@gmail.com>
2022-10-21 15:27:48 -0300

    comit do codigo teste

---
+++ test/app.test.js
@@ -0,0 +1,42 @@
+const request = require ("supertest")
+const { describe } = require("yargs")
+const app = require ("../app")
+
+describe("Realizando meu teste API", () =>{
+    it ("Soma de dois numeros",async()=>{
+        const res =await request(app).get("/somar")
+        .send({
+            "1num": 8,
+            "2num": 7
+        })
+            expect(res.statusCode).toEqual(200) 
+            expect(res.body).toEqual({"soma":15})
+    }),
+    it ("Subtração de dois numeros",async()=>{
+        const res =await request(app).get("/subtrair")
+        .send({
+            "1num": 8,
+            "2num": 7
+        })
+            expect(res.statusCode).toEqual(200) 
+            expect(res.body).toEqual({"subtracao":1})
+    }),
+    it ("Multiplicação de dois numeros",async()=>{
+        const res =await request(app).get("/multiplicar")
+        .send({
+            "1num": 8,
+            "2num": 7
+        })
+            expect(res.statusCode).toEqual(200) 
+            expect(res.body).toEqual({"multiplicar":56})
+    }),
+    it ("Divisão de dois numeros",async()=>{
+        const res =await request(app).get("/dividir")
+        .send({
+            "1num": 10,
+            "2num": 2
+        })
+            expect(res.statusCode).toEqual(200) 
+            expect(res.body).toEqual({"divisao":5})
+    })
+})
